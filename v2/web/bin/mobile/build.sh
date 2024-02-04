331096952a148fabb3064e5235348137efcbce13
Ross Alexandra <Ross-Alexandra@outlook.com>
2024-02-04 11:47:03 -0800

    Fixes to the build script & runs a build

--- v2/web/bin/mobile/build.sh
+++ v2/web/bin/mobile/build.sh
@@ -1,5 +1,6 @@
 source $IIMB_HOME/bin/activate;
 
+export LOCAL_IP=0.0.0.0; # This isn't used in the build process, but it's required.
 export VUE_APP_AUTH_URL=https://api.isitmybirth.day/auth;
 export VUE_APP_STREAKS_URL=https://api.isitmybirth.day/streaks;
 export VUE_APP_ENV=production;
