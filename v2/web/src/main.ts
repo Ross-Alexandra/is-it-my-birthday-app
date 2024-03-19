4d3dcf1106df31eaecaed28f660a9c5401a0d165
Ross Alexandra <ross-alexandra@outlook.com>
2024-03-18 18:08:13 -0700

    Fixes a bug where resuming apps could be out of date

--- v2/web/src/main.ts
+++ v2/web/src/main.ts
@@ -2,6 +2,16 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia';
 import App from './App.vue'
 import router from './router';
+import { Capacitor } from '@capacitor/core';
+import { App as CapacitorApp, type AppState } from '@capacitor/app';
+
+CapacitorApp.addListener('appStateChange', (state: AppState) => {
+    // If the app is coming back from the background, refresh the page
+    // to ensure we have the latest data.
+    if (state.isActive && Capacitor.isNativePlatform()) {
+        window.location.reload();
+    }
+});
 
 const pinia = createPinia();
 const app = createApp(App);
